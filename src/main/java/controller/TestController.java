701ee9b72a8b0f6ad8f25c02d8ba40752a970f79
Sedera10 <sederavalisoarandrianantenaina@gmail.com>
2025-11-13 19:43:20 +0300

    Sprint 4

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,7 +1,9 @@
 package controller;
 
 import myframework.annotation.MyMapping;
+import myframework.annotation.MyController;
 
+@MyController
 public class TestController {
 
     @MyMapping(url = "/bonjour")
@@ -12,5 +14,11 @@ public class TestController {
     @MyMapping(url = "/aurevoir")
     public void direAurevoir() {
         System.out.println("Au revoir !");
+    }
+
+    @MyMapping(url = "/ecrire")
+    public String ecrireChose() {
+        String message = "Voici une chose écrite par TestController. (Vita sprint 4)";
+        return message;
     }
 }
