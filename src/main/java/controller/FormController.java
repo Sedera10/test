4ad24942bca44a808ecc220a0216af72a83d320f
Sedera10 <sederavalisoarandrianantenaina@gmail.com>
2025-12-01 19:04:38 +0300

    Sprint 8 bis Teste

--- src/main/java/controller/FormController.java
+++ src/main/java/controller/FormController.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
+import model.Employe;
+
 @MyController
 public class FormController {
 
@@ -25,5 +27,20 @@ public class FormController {
 
         return "OK";
     }
+
+    @POST
+    @MyMapping("/employe/save")
+    public String save(Employe employe) {
+        System.out.println("====== EMPLOYE RECU ======");
+        System.out.println("Nom : " + employe.getNom());
+        System.out.println("Prenom : " + employe.getPrenom());
+        System.out.println("Poste : " + employe.getPoste());
+        System.out.println("Dtn : " + employe.getDtn());
+        System.out.println("Sexe : " + employe.getSexe());
+        System.out.println("Actif : " + employe.isActif());
+        System.out.println("Departement : " + employe.getDepartement().getNom());
+        return "OK";
+    }
+
 
 }
